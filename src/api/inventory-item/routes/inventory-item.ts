bb513089e628f1bac89fb5908bed26c2a9a9bea8
William Lyman <luke.sauls279@gmail.com>
2025-12-17 15:11:35 -0700

    added custom endpoints for selling, bulk selling, and restocking

--- src/api/inventory-item/routes/inventory-item.ts
+++ src/api/inventory-item/routes/inventory-item.ts
@@ -2,6 +2,55 @@
  * inventory-item router
  */
 
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::inventory-item.inventory-item');
+export default {
+  routes: [
+    {
+      method: "GET",
+      path: "/inventory-items",
+      handler: "inventory-item.find",
+      config: { auth: false },
+    },
+    {
+      method: "GET",
+      path: "/inventory-items/:id",
+      handler: "inventory-item.findOne",
+      config: { auth: false },
+    },
+    {
+      method: "POST",
+      path: "/inventory-items",
+      handler: "inventory-item.create",
+      config: { auth: true },
+    },
+    {
+      method: "PUT",
+      path: "/inventory-items/:id",
+      handler: "inventory-item.update",
+      config: { auth: true },
+    },
+    {
+      method: "DELETE",
+      path: "/inventory-items/:id",
+      handler: "inventory-item.delete",
+      config: { auth: true },
+    },
+    {
+      method: "POST",
+      path: "/inventory-items/sell",
+      handler: "inventory-item.sell",
+      config: { auth: true },
+    },
+    {
+      method: "POST",
+      path: "/inventory-items/restock",
+      handler: "inventory-item.restock",
+      config: { auth: true },
+    },
+    {
+      method: "POST",
+      path: "/inventory-items/bulk-sell",
+      handler: "inventory-item.bulkSell",
+      config: { auth: true },
+    },
+  ],
+};
